e5288c31c3323997c22e807cf534c5a69b29cca0
ysamardak <usamardak@gmail.com>
2024-11-21 08:10:47 -0500

    commit3

--- main.tf
+++ main.tf
@@ -30,15 +30,15 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
-
-resource "aws_iam_user" "test_user_99" {
-  name = "test-user-99"
-}
+#resource "aws_instance" "tc_instance" {
+#  ami           = "ami-0c7c4e3c6b4941f0f"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "TC-triggered-instance"
+#  }
+#}
+#
+#resource "aws_iam_user" "test_user_99" {
+#  name = "test-user-99"
+#}
